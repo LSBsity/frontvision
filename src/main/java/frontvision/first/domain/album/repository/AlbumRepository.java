03496e82057888605d44005294756a85de56d00a
LSBsity <dltmdqls51@naver.com>
2024-11-12 11:30:23 +0900

    fix(Album): Find All FetchJoin

--- src/main/java/frontvision/first/domain/album/repository/AlbumRepository.java
+++ src/main/java/frontvision/first/domain/album/repository/AlbumRepository.java
@@ -2,6 +2,12 @@ package frontvision.first.domain.album.repository;
 
 import frontvision.first.domain.album.domain.Album;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface AlbumRepository extends JpaRepository<Album, Long> {
+
+    @Query("select a from Album a join fetch a.artist")
+    List<Album> findAll();
 }
