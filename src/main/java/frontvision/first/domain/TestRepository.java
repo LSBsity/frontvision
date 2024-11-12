f2f586a26a4e242f3c6640bef58da6b761ab46fb
LSBsity <dltmdqls51@naver.com>
2024-11-12 11:17:10 +0900

    feat(Album): LOG

--- src/main/java/frontvision/first/domain/TestRepository.java
+++
@@ -1,6 +0,0 @@
-package frontvision.first.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<TestUser, Long> {
-}
--- src/main/java/frontvision/first/domain/album/controller/AlbumController.java
+++ src/main/java/frontvision/first/domain/album/controller/AlbumController.java
@@ -4,6 +4,7 @@ import frontvision.first.common.response.SuccessResponse;
 import frontvision.first.domain.album.dto.AlbumDto;
 import frontvision.first.domain.album.service.AlbumService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,14 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class AlbumController {
 
+    private static int count = 0;
     private final AlbumService albumService;
 
     @GetMapping("/albums")
     public ResponseEntity<SuccessResponse<List<AlbumDto.Response>>> getAlbums() {
+        log.info("getAlbum" + ++count);
         return SuccessResponse.of(
                 albumService.getAlbums()
         ).setStatus(HttpStatus.OK);
